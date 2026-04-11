07070c3bec4290369f43e9194bcc7c9076e69471
975269528 <975269528+users.noreply.github.com>
2026-04-11 12:43:52 +0800

    chore: 清理仓库，移除 node_modules/dist/备份/test/logs 等非必要文件

--- server/node_modules/pm2/bin/pm2.ps1
+++
@@ -1,3 +0,0 @@
-# pm2.ps1
-$pm2Path = Join-Path $PSScriptRoot "../lib/binaries/CLI.js"
-node $pm2Path $args
--- server/test_srv.js
+++
@@ -1,8 +0,0 @@
-const express=require('express'),path=require('path'),http=require('http'),fs=require('fs'),app=express();
-app.use(express.static(path.join(__dirname,'../client/dist')));
-app.use('/api',(req,res)=>{
-  const r=http.request({hostname:'127.0.0.1',port:3000,path:req.url,method:req.method,headers:{...req.headers}},pr=>{res.writeHead(pr.statusCode,pr.headers);pr.pipe(res)});
-  req.pipe(r);
-});
-app.get('*',(req,res)=>{res.sendFile(path.join(__dirname,'../client/dist/index.html'))});
-app.listen(3001,()=>console.log('TestSrv on 3001'));
